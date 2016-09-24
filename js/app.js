362e37c6243a92ce2ad9e082c7ec8ff0324cdacf
torin4fan <torin4fan@gmail.com>
2016-09-24 11:02:03 +0300

    2

--- js/app.js
+++ js/app.js
@@ -1,32 +1,48 @@
+var my_news = [
+
+];
 
 var App = React.createClass({
     render: function () {
         return (
-          <div className="app">
-              Всем привет, да опять
-              <News />
-          </div>
+            <div className="app">
+                Всем привет, да опять
+                <News data={my_news}/>
+            </div>
         );
     }
 });
 
 var News = React.createClass({
     render: function () {
+
+        var data = this.props.data;
+
+        if(data.length > 0){
+            var newsTamplate = data.map(function (item, index) {
+                return (
+                    <div key={index}>
+                        <p className="news__author">{item.author}</p>
+                        <p className="news__text">{item.text}</p>
+                    </div>
+                )
+            });
+        }
+        else {
+            newsTamplate = <p>Новостей нет</p>;
+        }
+
+
         return (
             <div className="news">
-                К сожалению, новостей нет.
+                {newsTamplate}
+                <strong className={data.length > 0 ? "":"none"}>
+                    Всего новостей: {data.length}
+                </strong>
             </div>
         )
     }
 });
-
-
-
-
-
-
-
-
 
 ReactDOM.render(
     <App />,
